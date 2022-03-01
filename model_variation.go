fc5cf0ec787b47da295c7f8629e3e24ea24b655f
Jamie Sinn <jamie.sinn@taplytics.com>
2022-03-01 13:52:58 -0500

    Fix variation variables to non-optional.

--- model_variation.go
+++ model_variation.go
@@ -8,15 +8,13 @@
  */
 package go_mgmt_sdk
 
-import "github.com/antihax/optional"
-
 type Variation struct {
 	// Unique key by Feature, can be used in the SDK / API to reference by 'key' rather than _id. Must only contain lower-case characters and `_` or `-`.
 	Key string `json:"key"`
 	// Variation display name.
 	Name string `json:"name"`
 	// A key-value map of variables to their value for this variation
-	Variables map[string]optional.Interface `json:"variables,omitempty"`
+	Variables map[string]interface{} `json:"variables,omitempty"`
 	// A unique Variation ID
 	Id string `json:"_id"`
 }
